15118e8454b629e905604679f55d4d5f5739aed9
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-05-15 17:13:27 +0200

    get dependencies.

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # THIS ONLY WORK IN OUR CI!
-docker run --rm -v /data/jenkins/jobs/docker-systemd-proxy-build/workspace:/usr/src/sproxy -w /usr/src/sproxy golang:1.4 go build -v
+docker run --rm -v /data/jenkins/jobs/docker-systemd-proxy-build/workspace:/usr/src/sproxy -w /usr/src/sproxy golang:1.4 /bin/bash -c 'go get -d && go build -v'
