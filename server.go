f9eec869feec5ee82e620f64ad68905d4eb5f72b
Andreas Litt <andreas.litt@gmail.com>
2015-05-20 17:15:53 +0200

    allow multiple UnitAction's per Key

--- server.go
+++ server.go
@@ -13,13 +13,21 @@ type UnitAction struct {
 	Unit   string
 }
 
-var ConfigData = map[string]UnitAction{
-	"ptw/nodename-DELETE": UnitAction{Action: "stop", Unit: "ptw-protonet.service"},
-	"ptw/nodename-PUT":    UnitAction{Action: "restart", Unit: "ptw-protonet.service"},
-	"ptw/nodename-DELETE": UnitAction{Action: "restart", Unit: "dokku-protonet.service"},
-	"ptw/nodename-PUT":    UnitAction{Action: "restart", Unit: "dokku-protonet.service"},
-	"ssh-DELETE":          UnitAction{Action: "restart", Unit: "dokku-protonet.service"},
-	"ssh-PUT":             UnitAction{Action: "restart", Unit: "dokku-protonet.service"},
+var ConfigData = map[string][]UnitAction{
+	"ptw/nodename-DELETE": []UnitAction{
+		UnitAction{Action: "stop", Unit: "ptw-protonet.service"},
+		UnitAction{Action: "restart", Unit: "dokku-protonet.service"},
+	},
+	"ptw/nodename-PUT": []UnitAction{
+		UnitAction{Action: "restart", Unit: "ptw-protonet.service"},
+		UnitAction{Action: "restart", Unit: "dokku-protonet.service"},
+	},
+	"ssh-DELETE": []UnitAction{
+		UnitAction{Action: "restart", Unit: "dokku-protonet.service"},
+	},
+	"ssh-PUT": []UnitAction{
+		UnitAction{Action: "restart", Unit: "dokku-protonet.service"},
+	},
 }
 
 func createHandler(connection *dbus.Conn) func(http.ResponseWriter, *http.Request) {
@@ -28,28 +36,29 @@ func createHandler(connection *dbus.Conn) func(http.ResponseWriter, *http.Reques
 		key := r.PostForm.Get("key")
 		action := r.PostForm.Get("action")
 		http_status := http.StatusOK
-		if unit_action, action_exists := ConfigData[key+"-"+action]; action_exists {
-			// TODO: Check if systemd unit exists
-			result_channel := make(chan string, 1)
-			var err error
-			var result string
-			switch unit_action.Action {
-			case "restart", "start":
-				_, err = connection.RestartUnit(unit_action.Unit, "fail", result_channel)
-			case "stop":
-				_, err = connection.StopUnit(unit_action.Unit, "fail", result_channel)
-			}
-			if err == nil {
-				result = <-result_channel
-			} else {
-				fmt.Printf("Systemd Unit ERROR: %s", err.Error())
-				w.Write([]byte(err.Error()))
-				http_status = http.StatusInternalServerError
-			}
-			if result != "done" {
-				fmt.Printf("Unexpected API result: %s", result)
-				w.Write([]byte(result))
-				http_status = http.StatusInternalServerError
+		if unit_actions, action_exists := ConfigData[key+"-"+action]; action_exists {
+			for _, unit_action := range unit_actions {
+				result_channel := make(chan string, 1)
+				var err error
+				var result string
+				switch unit_action.Action {
+				case "restart", "start":
+					_, err = connection.RestartUnit(unit_action.Unit, "fail", result_channel)
+				case "stop":
+					_, err = connection.StopUnit(unit_action.Unit, "fail", result_channel)
+				}
+				if err == nil {
+					result = <-result_channel
+				} else {
+					fmt.Printf("Systemd Unit ERROR: %s", err.Error())
+					w.Write([]byte(err.Error()))
+					http_status = http.StatusInternalServerError
+				}
+				if result != "done" {
+					fmt.Printf("Unexpected API result: %s", result)
+					w.Write([]byte(result))
+					http_status = http.StatusInternalServerError
+				}
 			}
 		} else {
 			http_status = http.StatusNotFound
