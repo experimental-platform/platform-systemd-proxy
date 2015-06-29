e40def6b43d0a918a96654abc68039e7ff12a6d2
Hinnerk Haardt <hinnerk@randnotizen.de>
2015-06-29 16:41:30 +0200

    More universal build (still only in our CI).

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# THIS ONLY WORK IN OUR CI!
-docker run --rm -v /data/jenkins/jobs/${JOB_NAME}/workspace:/usr/src/sproxy -w /usr/src/sproxy golang:1.4 /bin/bash -c 'go get -d && go build -v'
+set -e
+
+SRC_PATH=$(pwd)
+
+# we're calling docker from within a container with a path (/data/jenkins) mounted into this container (/var/jenkins)
+# so the newly created container needs a different path (/data/jenkins).
+docker run --rm -v /data${SRC_PATH#/var}:/usr/src/sproxy -w /usr/src/sproxy golang:1.4 /bin/bash -c 'go get -d && go build -v'
