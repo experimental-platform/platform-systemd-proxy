91c1c74c42d07c77bdef1fdf5ad56dad08f63cec
Hinnerk Haardt <haardt@information-control.de>
2015-08-12 12:45:32 +0200

    Initial CI configuration.

--- ci-build.sh
+++ ci-build.sh
@@ -3,6 +3,4 @@ set -e
 
 SRC_PATH=$(pwd)
 
-# we're calling docker from within a container with a path (/data/jenkins) mounted into this container (/var/jenkins)
-# so the newly created container needs a different path (/data/jenkins).
-docker run --rm --volume=/data${SRC_PATH#/var}:/usr/src/sproxy --workdir=/usr/src/sproxy golang:1.4 /usr/src/sproxy/patch_dbus.sh
+docker run --volume=${SRC_PATH}:/usr/src/sproxy --workdir=/usr/src/sproxy golang:1.4 /usr/src/sproxy/patch_dbus.sh
