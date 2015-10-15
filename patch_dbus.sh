c4c51ba7ac27b11cf6eb59e8c052a10a4189d85b
Kamil Domański <kamil@domanski.co>
2015-10-15 17:36:26 +0200

    remove obsolete patch

--- patch_dbus.sh
+++ patch_dbus.sh
@@ -10,21 +10,3 @@ if [[ ${PIPESTATUS[0]} -eq 0 ]]; then
     exit 0
 fi
 
-# yeah, no patch...
-apt-get update
-apt-get install -y patch
-# an unsuccessfull build might be caused by the dbus assertion bug
-# so we patch it w/ pull request #99 and then try again.
-# pull request: https://github.com/coreos/go-systemd/issues/98
-filename=$(go build -v 2>&1 | awk -F ':' '/dbus.go/ {print $1}')
-echo "Filemname: ${filename}"
-hash=$(md5sum ${filename}|awk '{print $1}')
-echo "Hash: ${hash}"
-if [[ ${hash} == 3b75aae9d8a95a39d84b4c2160d938a2 ]]; then
-    cp 99.patch $(dirname ${filename})
-    cd $(dirname ${filename})
-    patch -p 1 dbus.go < 99.patch
-    cd -
-    go build -v
-fi
-
